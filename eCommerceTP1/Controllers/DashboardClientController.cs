8905d4e3f02b4ce164f12388e967cf5043160bc0
Lylia15000 <lyliaamiour@gmail.com>
2026-03-01 14:50:12 -0500

    - Fixer la navBarre - Ajouter des filitres dans la page DashboardClient - Ajout de la page voir détail du produit + stulisée.

--- eCommerceTP1/Controllers/DashboardClientController.cs
+++ eCommerceTP1/Controllers/DashboardClientController.cs
@@ -14,7 +14,7 @@ namespace eCommerceTP1.Controllers
             _httpClient = new HttpClient();
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string search = "", string category = "")
         {
             var response = await _httpClient.GetStringAsync("https://dummyjson.com/products");
 
@@ -23,9 +23,9 @@ namespace eCommerceTP1.Controllers
                 PropertyNameCaseInsensitive = true
             });
 
-            // Conversion DummyProduct → Produit
             var produits = data?.Products.Select(p => new Produit
             {
+                Id = p.Id,
                 Title = p.Title,
                 Description = p.Description,
                 Price = p.Price,
@@ -33,7 +33,48 @@ namespace eCommerceTP1.Controllers
                 Image = p.Images?.FirstOrDefault()
             }).ToList();
 
+            // Filtrage
+            if (!string.IsNullOrEmpty(search))
+            {
+                produits = produits.Where(p =>
+                    p.Title.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                produits = produits.Where(p =>
+                    p.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            // Liste des catégories pour le dropdown
+            ViewBag.Categories = produits?.Select(p => p.Category).Distinct().ToList();
+
             return View(produits);
+        }
+        public async Task<IActionResult> DetailProduit(int id)
+        {
+            var response = await _httpClient.GetStringAsync($"https://dummyjson.com/products/{id}");
+
+            var data = JsonSerializer.Deserialize<Produit>(response, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            if (data == null)
+                return NotFound();
+
+            // Mapping vers le modèle produit
+            var produit = new Produit
+            {
+                Id = data.Id,
+                Title = data.Title,
+                Description = data.Description,
+                Price = data.Price,
+                Category = data.Category,
+                Image = data.Images?.FirstOrDefault()
+            };
+
+            return View(produit);
         }
     }
 }
