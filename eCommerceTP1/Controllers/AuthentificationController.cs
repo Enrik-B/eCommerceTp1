5cc3b48041d08fe8969dbc8ca6447298aee388f3
Lylia15000 <lyliaamiour@gmail.com>
2026-03-01 01:26:43 -0500

    Ajout du controller authentification

---
+++ eCommerceTP1/Controllers/AuthentificationController.cs
@@ -0,0 +1,53 @@
+﻿using eCommerceTP1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceTP1.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        private readonly eCommerceTP1DbContext _context;
+
+        public AuthentificationController(eCommerceTP1DbContext context)
+        {
+
+            _context = context;
+        }
+        public IActionResult Inscription()
+        { return View(); }
+
+        [HttpPost]
+        public IActionResult Inscription(User user)
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login() { return View(); }
+        [HttpPost]
+        public IActionResult Login(string Identifier, string Password)
+        {
+            var user = _context.Users.FirstOrDefault(u => (u.Email == Identifier || u.Username == Identifier));
+            if (user == null)
+            {
+                ViewBag.Error = "Utilisateur introuvable.";
+                return View();
+            }
+            bool passwordOk = BCrypt.Net.BCrypt.Verify(Password, user.Password);
+            if (!passwordOk)
+            {
+                ViewBag.Error = "Mot de passe incorrect.";
+                return View();
+            }
+            // Stockage en session
+            HttpContext.Session.SetString("UserId", user.Id.ToString());
+            HttpContext.Session.SetString("UserRole", user.Role);
+            // Retourner une phrase au lieu d'une redirection
+            return Content($"Connecté en tant que {user.Role}"); 
+        } 
+    }
+
+}   
+
